f477ba37bfc8881616ccd01dacd5d0d47715e7ed
Ilya Mihaylov <strander2@mail.ru>
2018-10-08 23:41:44 +0300

    Added 6.1.1

--- lr6/lab6.swift
+++ lr6/lab6.swift
@@ -1,3 +1,35 @@
 import Foundation
 
 print("Task 1: ")
+
+extension Int {
+    func even() -> Bool {
+        return self%2 == 0
+    }
+    
+    func odd() -> Bool {
+        return self%2 == 1
+    }
+    
+    subscript(digit: Int) -> Int {
+        var count = 0
+        var number = self
+        
+        while(number>=1){
+            if(number%10 == digit) {
+                count += 1
+            }
+            number = number/10
+        }
+        
+        return count
+    }
+}
+
+print(3.even())
+print(3.odd())
+
+print(4.even())
+print(4.odd())
+
+print(113312222[2])
